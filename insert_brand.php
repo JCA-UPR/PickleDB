e1294e24f66ad73065ce51353a43e94e911df98a
PapaJuly <juliansoto1130@gmail.com>
2024-05-10 18:29:16 -0400

    fix connect errors

--- insert_brand.php
+++ insert_brand.php
@@ -18,9 +18,10 @@ $ceo = $_POST['ceo'];
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+      }
+      
 
 
     // Prepare SQL statement to insert data into brands table
