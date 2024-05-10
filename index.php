c9c6632a5531aa41aecf73a3fd50bbc706e662a6
PapaJuly <juliansoto1130@gmail.com>
2024-05-10 18:14:13 -0400

    changed connection type

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
         $dbname = "mS224DB_juliansp";
         
         // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
         // Check connection
         if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
